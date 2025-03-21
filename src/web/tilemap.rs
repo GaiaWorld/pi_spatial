34274671466679e6ed003ce79c803edb1db82e32
zhuy <zhuy>
2025-03-21 18:39:06 +0800

    feat: 编译错误

--- src/web/tilemap.rs
+++ src/web/tilemap.rs
@@ -1,8 +1,9 @@
 use nalgebra::Point2;
 use parry2d::bounding_volume::Aabb;
+use parry2d::bounding_volume::Aabb as AABB;
 use pi_slotmap::{SlotMap, DefaultKey, Key, KeyData};
 use wasm_bindgen::prelude::wasm_bindgen;
-
+use nalgebra::Vector2;
 use crate::quad_helper::intersects;
 use crate::tilemap::TileMap as TileMapInner;
 use super::quad_tree::{AbQueryArgs, ab_query_func};
@@ -42,12 +43,13 @@ impl TileMapTree {
         self.0.shift(DefaultKey::from(KeyData::from_ffi(id as u64)), Vector2::new(x, y));
     }
     pub fn move_to(&mut self, id: f64, x: f32, y: f32) {
-        self.0.move_to(DefaultKey::from(KeyData::from_ffi(id as u64)), Vector2::new(x, y));
+        self.0.move_to(DefaultKey::from(KeyData::from_ffi(id as u64)), Point2::new(x, y));
     }
 
     pub fn query(&self, min_x: f32, min_y: f32, max_x: f32, max_y: f32,) -> Vec<f64> {
         let min = Point2::new(min_x, min_y);
         let max = Point2::new(max_x, max_y);
+        let ab = AABB::new(min, max);
         let mut args = AbQueryArgs::new(ab, usize::MAX);
         self.0.query(&AABB::new(min, max), &mut args, ab_query_func);
         args.result
